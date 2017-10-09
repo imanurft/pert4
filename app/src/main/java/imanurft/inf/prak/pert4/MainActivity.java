17e36cb81a85e48b09726b6019df6b5c06573875
imanurft <imanurfitriani@gmail.com>
2017-10-09 17:34:38 +0700

    menambah main activity.java dan hi ima pada activity_main.xml

--- app/src/main/java/imanurft/inf/prak/pert4/MainActivity.java
+++ app/src/main/java/imanurft/inf/prak/pert4/MainActivity.java
@@ -2,12 +2,17 @@ package imanurft.inf.prak.pert4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.TextView;
+//ini program imanurft
 public class MainActivity extends AppCompatActivity {
+
+    TextView teks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teks = (TextView) findViewById(R.id.teks);
     }
 }
